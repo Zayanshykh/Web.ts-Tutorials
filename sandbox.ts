d0ea5edae02b8e732a087a9302d4d1bd78ff3d0e
Zayan <160765765+Zayanshykh@users.noreply.github.com>
2024-07-04 10:07:06 -0700

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'Zayan.shaikh';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  console.log(input);
+});
---
+++ studio.ts
@@ -0,0 +1,13 @@
+//                           TypeScript 101
+
+//       -AlterNative to JavaScript (superset)
+//       -Allows us to use strict types 
+//       -Supports modern features (Arrow Function, let , const)
+//       -Extra features (generics,interface,tuples etc)
+
+//                          You Should Khnow...
+
+//   -JavaScript
+//   -ArrowFunctions
+//   -The DOM
+//   -Classes
